526b89cf837ff261096f19f74a06004d1a017576
Frank Villaro-Dixon <frank@vi-di.fr>
2014-06-07 16:05:18 +0200

    stupid matlab comparison

--- src/signals/Signal_assert_type.m
+++ src/signals/Signal_assert_type.m
@@ -2,7 +2,7 @@ function Signal_assert_type(Signal, nameWanted)
 % Checks that the signal given on the input is of the type nameWanted
 % This function is mainly used by SSS_assert_type(Sig), with params Sig and SSS.
 
-if(nargin != 2)
+if(nargin ~= 2)
 	%If fucking matlab was clever, they'd have implemented print_usage, like octave
 	error('Usage: Signal_assert_type(Signal, nameWanted)')
 end

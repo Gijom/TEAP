102ef8fd8b7e032541caea33f53ea76118057048
Frank Villaro-Dixon <frank@vi-di.fr>
2014-06-25 23:44:00 +0200

    automatic conversion siemens - ohms

--- src/signals/GSR/acquisition/GSR_aqn_variable.m
+++ src/signals/GSR/acquisition/GSR_aqn_variable.m
@@ -10,6 +10,13 @@ Signal = GSR_new_empty();
 
 Signal.sampFreq = sampFreq; %Hz
 
+if(median(raw) < 1)
+	warning(['The signal given seems to be given in Siemens. I need Ohms. ' ...
+             'Automatic conversion applied']);
+	raw = 1/raw;
+end
+
+
 Signal.raw = Raw_convert_1D(GSR);
 
 

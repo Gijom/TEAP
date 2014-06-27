2582633ca62ef6e90a76beb8b0318d78a726aec7
Frank Villaro-Dixon <frank@vi-di.fr>
2014-06-27 23:52:03 +0200

    visualisation with axis and co

--- src/signals/visualisation/Signal_plot.m
+++ src/signals/visualisation/Signal_plot.m
@@ -1,16 +1,33 @@
 function Signal_plot(Signal, startT, endT)
-%Plots a signal: value vs time, between startT and entT
+%Plots a signal: value vs time, between startT and entT.
+% Signal_plot(sig); Signal_plot(sig, start); Signal_plot(sig, start, end);
 % Inputs:
 %  Signal: the sigal to plot
-%  startT: the start time, in frames
-%  endT: the end time, or 0 if all the signal
+%  startT: the start time, in frames (optional)
+%  endT: the end time, or 0 if all the signal (optional)
 %
 %Copyright Frank Villaro-Dixon Creative Commons BY-SA 4.0 2014
 
 raw = Signal_get_raw(Signal);
+samprate = Signal_get_samprate(Signal);
+name = Signal_get_name(Signal);
+signal_unit = Signal_get_unit(Signal);
 
-if(endT ~= 0)
-	raw = raw(1:endT);
+if(nargin == 2)
+	raw = raw(startT:end);
+elseif(nargin == 3)
+	raw = raw(startT:endT);
 end
 
-plot(raw);
+
+xes = [1:length(raw)];
+
+seconds = xes ./ samprate;
+
+plot(seconds, raw);
+
+xlabel('Seconds');
+ylabel([name ' (' signal_unit ')']);
+title([name ' vs Seconds']);
+
+

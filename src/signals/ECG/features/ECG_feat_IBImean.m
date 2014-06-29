268aba9fef45a4971becd8e19d054ffa5004d825
Frank Villaro-Dixon <frank@vi-di.fr>
2014-06-29 14:37:50 +0200

    ECG things

--- src/signals/ECG/features/ECG_feat_IBImean.m
+++ src/signals/ECG/features/ECG_feat_IBImean.m
@@ -1,29 +1,35 @@
-function [IBIMean ECGSignal] = ECG_feat_IBImean(ECGsignal)
+function [IBIMean ECGSignal] = ECG_feat_IBImean(ECGSignal)
 %Computes the InterBeatInterval mean of an ECG signal
+% Inputs:
+%  ECGSignal: the ECG signal
+% Outputs:
+%  IBIMean: the InterBeatInterval mean
+%  ECGSignal: the ECG signal with the embedded feature
 %
 %Copyright Guillaume Chanel 2013
 %Copyright Frank Villaro-Dixon Creative Commons BY-SA 4.0 2014
 
 
 %Make sure we have an ECG signal
-ECG_assert_type(ECGsignal)
+ECG_assert_type(ECGSignal)
 
 
 %If the features were already computed
-if(isfield(GSRsignal, 'IBIMean'))
-	Mean = ECGsignal.IBIMean;
+if(Signal_has_feature(ECGSignal, 'IBIMean'))
+	feat = Signal_get_feature(ECGSignal, 'IBIMean');
+	IBIMean = feat.value;
 	return;
 end
 
 %Compute the results
 
-rawSignal = Signal_get_raw(ECGsignal);
-signalSampFreq = Signal_get_samprate(ECGsignal);
+rawSignal = Signal_get_raw(ECGSignal);
+signalSampFreq = Signal_get_samprate(ECGSignal);
 
 newfs = 256; %Hz, as needed by rpeakdetect
 ECG = downsample(rawSignal, signalSampFreq/newfs);
 [hrv, R_t, R_amp, R_index, S_t, S_amp] = rpeakdetect(ECG, newfs);
 [BPM IBI] = correctBPM(R_index, newfs);
 
-ECGSignal.IBIMean = mean(IBI);
-IBIMean = ECGSignal.IBIMean;
+IBIMean = mean(IBI);
+ECGSignal = Signal_set_feature('IBIMean', IBIMean);
--- src/signals/ECG/features/ECG_feat_IBIvar.m
+++ src/signals/ECG/features/ECG_feat_IBIvar.m
@@ -1,29 +1,36 @@
-function [IBIVar ECGSignal] = ECG_feat_IBIvar(ECGsignal)
+function [IBIVar ECGSignal] = ECG_feat_IBIvar(ECGSignal)
 %Computes the InterBeatInterval variance of an ECG signal
+% Inputs:
+%  ECGSignal: the ECG signal
+% Outputs:
+%  IBIVar: the InterBeatInterval variance
+%  ECGSignal: the ECG signal with the embedded feature
 %
 %Copyright Guillaume Chanel 2013
 %Copyright Frank Villaro-Dixon Creative Commons BY-SA 4.0 2014
 
 
 %Make sure we have an ECG signal
-ECG_assert_type(ECGsignal)
+ECG_assert_type(ECGSignal)
 
 
 %If the features were already computed
-if(isfield(GSRsignal, 'IBIVar'))
-	Mean = ECGsignal.IBIVar;
+if(Signal_has_feature(ECGSignal, 'IBIVar'))
+	feat = Signal_get_feature(ECGSignal, 'IBIVar');
+	IBIVar = feat.value;
 	return;
 end
 
+
 %Compute the results
 
-rawSignal = Signal_get_raw(ECGsignal);
-signalSampFreq = Signal_get_samprate(ECGsignal);
+rawSignal = Signal_get_raw(ECGSignal);
+signalSampFreq = Signal_get_samprate(ECGSignal);
 
 newfs = 256; %Hz, as needed by rpeakdetect
 ECG = downsample(rawSignal, signalSampFreq/newfs);
 [hrv, R_t, R_amp, R_index, S_t, S_amp] = rpeakdetect(ECG, newfs);
 [BPM IBI] = correctBPM(R_index, newfs);
 
-ECGSignal.IBIVar = var(IBI);
-IBIVar = ECGSignal.IBIVar;
+IBIVar = var(IBI);
+ECGSignal = Signal_set_feature('IBIVar', IBIVar);

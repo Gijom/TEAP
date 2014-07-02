8ac0c80ab23019e0dfa4ff942b09a4c782de8788
Frank Villaro-Dixon <frank@vi-di.fr>
2014-07-02 14:56:05 +0200

    extract from .ascii bdf. works good

--- src/signals/scratch_utils/extractSignalFromData.sh
+++ src/signals/scratch_utils/extractSignalFromData.sh
@@ -2,8 +2,17 @@
 
 #Extracts a particular signal from an 'ASCII generated' data from edfbrowser
 
+cat Participant*_signals.txt  | cut -f1 -d' ' | tail -n +2
+
+
+echo -n "Which signal do you want > "
+read signal
+
+signVal=$(cat Participant*_signals.txt  | cut -f1 -d' ' | tail -n +2 | grep $signal | cut -d, -f2)
+
+
+
 dataFile=Participant*_data.txt
-signal=45 #RES
 signal=$(($signal + 1)) #time
 
-cat $dataFile | cut -d, -f $signal > outFile_$dataFile
+cat $dataFile | cut -d, -f $signal > outFile_signal_$signVal

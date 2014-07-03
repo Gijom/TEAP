6cec39d85aa34592c5868bca4afe84bd2cc7427b
Frank Villaro-Dixon <frank@vi-di.fr>
2014-07-04 00:58:24 +0200

    ECG example

---
+++ src/signals/ECG/examples/ECG_feat_IBI_example.m
@@ -0,0 +1,12 @@
+addpath(genpath('../../../'));
+
+probe1 = csvread('ECG_example_probe1.csv');
+probe2 = csvread('ECG_example_probe2.csv');
+
+sig = ECG_aqn_variable(probe1, probe2, 1024);
+
+IBImean = ECG_feat_IBImean(sig)
+BPMmean = 60/IBImean
+IBIvar = ECG_feat_IBIvar(sig)
+
+Signal_plot(sig);

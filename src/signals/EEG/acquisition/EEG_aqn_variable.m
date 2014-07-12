7ef4cc469ad010c5e5ed5dce758705f534325bb9
Frank Villaro-Dixon <frank@vi-di.fr>
2014-07-12 20:14:54 +0200

    EEG aqn variable

---
+++ src/signals/EEG/acquisition/EEG_aqn_variable.m
@@ -0,0 +1,29 @@
+function Signal = EEG_aqn_variable(eegChannels, eegData, samprate)
+% EEG_aqn_variable gets an EEG signal from a variable.
+% Inputs:
+%   eegChannels [Cxstrings]: a vector specifying the C EEG channels given
+%   eegData [CxN]: a matrix containing the EEG data
+%   sampRate [1x1]: the sampling rate, in Hz
+% Outputs:
+%   Signal: An EEG TEAPhysio signal
+
+if(nargin ~= 3)
+	error('Usage: Signal = EEG_aqn_variable(eegChannels, eegData, sampRate);
+end
+
+
+Signal = EEG_new_empty();
+Signal = Signal_set_samprate(Signal, sampRate);
+
+[nChannels, ~] = size(eegChannels);
+[nChannelsGiven, ~] = size(eegData);
+
+if(nChannelsGiven ~= nChannels)
+	error(['Hey, you said that you''d give me ' num2str(nChannels) ...
+	       'channels but you gave me ' num2str(nChannelsGiven) ' instead']);
+end
+
+for(i = [1:nChannels])
+	Signal = EEG_set_channel(Signal, eegChannels(i, :), eegData(i, :));
+end
+

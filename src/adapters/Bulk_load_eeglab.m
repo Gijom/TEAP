0b911353781396e121d371580bb378010b03d663
Frank Villaro-Dixon <frank@vi-di.fr>
2014-07-29 13:30:27 +0200

    Bulk load

--- src/adapters/Bulk_load_eeglab.m
+++ src/adapters/Bulk_load_eeglab.m
@@ -1,36 +1,49 @@
-function [BulkSig] = Bulk_load_eeglab(EEGLabVar)
+function [BulkSig] = Bulk_load_eeglab(EEGV)
 %Loads an EEGLab variable, into a TEAPhysio bulk signal, containing EEG, ECG,
 %GSR, etc.
 % Inputs:
-%  EEGLabVar: the variable given by EEGLab (ex: load A.mat; Bulk_load_eeglab(EEG))
+%  EEGV: the variable given by EEGLab (ex: load A.mat; Bulk_load_eeglab(EEG))
 % Outputs:
-%  BulkSig: a TEAPhysio bulk signal
+%  BulkSig: a TEAPhysio bulk signal vector
 %
 %Copyright Frank Villaro-Dixon Creative Commons BY-SA 4.0 2014
 
 if(nargin ~= 1)
-	error('Usage: BulkSig = Bulk_load_eeglab(EEGLabVar)');
+	error('Usage: BulkSig = Bulk_load_eeglab(EEGV)');
 end
 
-%Operate channel by channel
 
-BulkSig.tatat = 42;
+%Number of epochs:
+nEpochs = length(EEGV.epoch);
 
-for ichannel = [1:size(EEGLabVar.chanlocs)]
-	chanName = EEGLabVar.chanlocs(ichannel).labels;
 
-	BulkSig = processChannel(ichannel, BulkSig);
+%foreach epoch
+for iEpoch = [1:nEpochs]
+	clear Bulk;
+	Bulk.tatat = 42;
+
+	Bulk = addGSR(Bulk, iEpoch);
+
+	BulkSig(iEpoch) = Bulk;
 end
 
-function [BulkSig] = processChannel(ichannel, BulkSig)
-	disp(['I have ' num2str(ichannel) ' - ' EEGLabVar.chanlocs(ichannel).labels]);
-	chanName = EEGLabVar.chanlocs(ichannel).labels;
 
-	switch(chanName)
-	case 'GSR1'
-		GSRSig = GSR_aqn_variable(
-		Bulk
+
+%GSR
+function BulkSig = addGSR(BulkSig, iEpoch);
+	GSRChannel = 41; %FIXME: my examples don't work: negative RES
+	if(strcmp('GSR1', EEGV.chanlocs(GSRChannel).labels) ~= 1)
+		disp(['nonon: ' EEGV.chanlocs(GSRChannel).labels]);
+		return;
+	end
+
+	data = EEGV.data(GSRChannel, :, iEpoch);
+	reshaped = reshape(data, 1, length(data));
+	plot(reshaped)
+	GSRSig = GSR_aqn_variable(reshaped, EEGV.srate);
+	Bulk.(GSR_get_name()) = GSRSig;
 end
+
 
 
 end

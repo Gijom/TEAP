702018d88de774caf7cc154bca0c0eefc208fe9a
Frank Villaro-Dixon <frank@vi-di.fr>
2014-07-31 18:39:00 +0200

    BULK: RES

---
+++ src/signals/RES/RES_get_name.m
@@ -0,0 +1,12 @@
+function name = RES_get_name()
+%Get the name of a RES signal
+% Inputs:
+%  NONE
+% Outputs:
+%  name: the name of the RES signal, in this case, 'RES'
+%
+%Copyright Frank Villaro-Dixon Creative Commons BY-SA 4.0 2014
+
+name = 'RES';
+
+

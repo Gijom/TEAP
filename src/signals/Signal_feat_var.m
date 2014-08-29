b3ee03ed4d0ccf36458898dfc3ac9e0668c8f7b7
Frank Villaro-Dixon <frank@vi-di.fr>
2014-08-29 16:57:36 +0200

    Signal_feat_var

---
+++ src/signals/Signal_feat_var.m
@@ -0,0 +1,16 @@
+function [var] = Signal_feat_var(Signal)
+%Computes the variance of a signal
+% Inputs:
+%  Signal
+% Outputs:
+%  var: the calculated var, using the var() function
+%
+%Copyright Frank Villaro-Dixon Creative Commons BY-SA 4.0 2014
+
+Signal_assert_mine(Signal);
+
+raw = Signal_get_raw(Signal);
+
+var = var(raw);
+
+

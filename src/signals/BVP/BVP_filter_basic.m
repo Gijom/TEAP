4b388e0568477dab3fc4ee3612cc0bf746855107
Frank Villaro-Dixon <frank@vi-di.fr>
2014-09-04 21:40:35 +0200

    BVP BPMs

---
+++ src/signals/BVP/BVP_filter_basic.m
@@ -0,0 +1,18 @@
+function BVPsignal = BVP_filter_basic(BVPsignal)
+%Cleans a signal adding a low-pass median filter to it. The window equals the
+%sample rate, aka 1 sec.
+% Inputs:
+%  BVPsignal: the BVP signal.
+% Outputs:
+%  Signal: the filtered BVP signal
+%
+%Copyright Frank Villaro-Dixon Creative Commons BY-SA 4.0 2014
+
+
+%Make sure we have a BVP signal
+BVPsignal = BVP_assert_type(BVPsignal);
+
+sampRate = Signal_get_samprate(BVPsignal);
+
+BVPsignal = Signal_filter1_low_median(BVPsignal, sampRate);
+
---
+++ src/signals/BVP/features/BVP_feat_BPM.m
@@ -0,0 +1,87 @@
+function BPM = BVP_feat_BPM(BVPSignal)
+%Computes the BPM from a BVP signal
+% Inputs:
+%  BVPs: the BVP signal
+% Outputs:
+%  BPM: the beats per minute
+%
+%Copyright Mohammad Soleymani 2013
+%Copyright Frank Villaro-Dixon Creative Commons BY-SA 4.0 2014
+
+
+%Make sure we have an BVP signal
+BVPSignal = BVP_assert_type(BVPSignal);
+
+data = Signal_get_raw(BVPSignal)';
+fs = Signal_get_samprate(BVPSignal);
+
+
+if(nargin < 3)
+    methodPeak = 'max';
+end
+
+SizeWindow = round(fs/50);
+data = data - mean(data);
+
+%Filter the data
+dataS = filtfilt(ones(1, SizeWindow)/SizeWindow, 1,
+                 [repmat(data(1), SizeWindow, 1); data]);
+dataS = dataS(SizeWindow+1:end);
+
+diffS = diff(dataS);
+
+%recherche des pics postif : deriv decroissante = 0
+listePic = [];
+for(iSpl = 1:length(diffS)-1)
+	%si il y a une derive == 0 sur le dernier sample elle n'est pas prise en compte
+	if((diffS(iSpl) > 0) && (diffS(iSpl+1) < 0))
+		listePic = [listePic iSpl+(diffS(iSpl) / (diffS(iSpl) - diffS(iSpl+1)))];
+	elseif((diffS(iSpl) == 0) && (diffS(iSpl+1) < 0))
+		listePic = [listePic iSpl];
+	end
+end
+
+%Sure to keep that ?
+listePic = round(listePic);
+
+
+%Procedure to keep only peaks that are separated by at least 0.5 seconds
+%other are considered as the same peak and one of the two is selected
+%according to the chosen method. Also peaks that lie alone in the
+%first 0.5 seconds are removed (cannot determine which peak it is…)
+limit = round(0.5*fs);
+
+%Remove too early first peakS
+if((listePic(1) < limit) && (listePic(2) - listePic(1) >= limit))
+	listePic = listePic(2:end);
+end
+
+%remove other peaks
+iPic = 1;
+while(iPic <= length(listePic) - 1)
+	%If two peaks are too close keep the one depending on the method
+	if((listePic(iPic+1) - listePic(iPic)) < limit)
+		[dummy, choice] = max([data(listePic(iPic)) data(listePic(iPic+1))]);
+
+		if(choice == 1)
+			listePic(iPic+1) = [];
+		else
+			listePic(iPic) = [];
+		end
+	else
+		iPic = iPic + 1;
+	end
+end
+
+
+if(length(listePic) < 2)
+	error('There should be at least 2 peaks to detect');
+end
+
+%Compute bpm from the pic list
+[BPM delta_t t] = PICtoBPM(listePic, fs);
+
+BPM = mean(BPM); %And take it's mean
+
+end
+

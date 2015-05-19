8953bbf95c50fb40c7273a315ce172f419a7655e
soleyman <soleyman@e45a9c4b-0fe8-4931-b830-b1ab2d33adfa>
2015-05-19 14:23:48 +0000

    git-svn-id: https://subversion.unige.ch/users/soleyman/TEAP@11 e45a9c4b-0fe8-4931-b830-b1ab2d33adfa

--- src/signals/ECG/features/ECG_feat_extr.m
+++ src/signals/ECG/features/ECG_feat_extr.m
@@ -3,19 +3,20 @@ function [ECG_features] = ECG_feat_extr(ECGSignal)
 % Inputs:
 %  ECGSignal: the ECG signal
 % Outputs:
-%  ECG_features: 1x19 vector including in the order of appearance in the 
+%  ECG_features: 1x17 vector including in the order of appearance in the 
 %vector:
-%1. HRV variance, 2. mean HRV, 3. mean heart rate, 4-8. Multiscale entropy at 5 levels on
-%HRV (5 feaures), 9. spectral powere 0-0.1Hz, 10.spectral power 0.1-0.2Hz,
-%11. spectral power 0.2-0.3 12. spectral power 0.3-0.4Hz,
-%13. spectral energy ratio between f<0.08Hz/f>0.15Hz and f<5.0Hz
-%14. Low frequency PS in tachogram (HRV) 
-%15. LF
-%16.MF
-%17. HF
-%18. (MF/(LF+HF))
-%19. sp01504
-% 
+%1. HRV variance, 2. mean HRV, 3. mean heart rate, 
+%4-8. Multiscale entropy at 5 levels on HRV (5 feaures), 
+%9. Spectral power 0-0.1Hz, 
+%10. Spectral power 0.1-0.2Hz,
+%11. Spectral power 0.2-0.3 
+%12. Spectral power 0.3-0.4Hz,
+%13. Spectral energy ratio between f<0.08Hz/f>0.15Hz and f<5.0Hz
+%14. Low frequency spectral power in tachogram (HRV)  [0.01-0.08Hz]
+%15. Medium frequency spectral power in tachogram (HRV)  [0.08-0.15Hz]
+%16. High frequency spectral power in tachogram (HRV)  [0.15-0.5Hz]
+%17. Energy ratio for tachogram spectral content (MF/(LF+HF))
+ 
 %Copyright Guillaume Chanel 2013
 %Copyright Frank Villaro-Dixon, BSD Simplified, 2014
 
@@ -48,6 +49,11 @@ sp0203 = log(sum(P(f>0.2 & f<=0.3))+eps);
 sp0304 = log(sum(P(f>0.3 & f<=0.4))+eps);
 energyRatio = log(sum(P(f<0.08))/sum(P(f>0.15 & f<5))+eps);
 %tachogram features; psd features on inter beat intervals
+%R. McCraty, M. Atkinson, W. Tiller, G. Rein, and A. Watkins, “The
+%effects of emotions on short-term power spectrum analysis of
+%heart rate variability,” The American Journal of Cardiology, vol. 76,
+%no. 14, pp. 1089 – 1093, 1995
+
 tachogram = zeros(length(ECG),1);
 tachogram(1:round(t(1)*samprate)) = IBI(1);
 for i = 1:length(t)-1
@@ -59,12 +65,10 @@ clear tachogram
 LF = log(sum(Pt(ft>0.01 & ft<=0.08))+eps);
 MF = log(sum(Pt(ft>0.08 & ft<=0.15))+eps);
 HF = log(sum(Pt(ft>0.15 & ft<=0.5))+eps);
-sp01504 = log(sum(Pt(ft>0.15 & ft<=0.4))+eps); %rainville
 
-
-ECG_features =[IBIVar HRV HR MSE ... 1:9
-    sp0001 sp0102 sp0203 sp0304 energyRatio ... 10:14
-    LF MF HF (MF/(LF+HF)) sp01504];  %15:19
+ECG_features =[IBIVar HRV HR MSE ... 1:8
+    sp0001 sp0102 sp0203 sp0304 energyRatio ... 9:13
+    LF MF HF (MF/(LF+HF))];  %14:17
 
 
 

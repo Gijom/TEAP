21f9a6347c193edb15e6169b1ff86ca6d78b55b4
Mohammad Soleymani <mohammad.soleymani@unige.ch>
2015-11-17 10:35:42 +0100

    added log

--- src/signals/EEG/features/EEG_feat_bandENR.m
+++ src/signals/EEG/features/EEG_feat_bandENR.m
@@ -27,19 +27,21 @@ alphaBand = zeros(1, n_electrodes);
 betaBand  = zeros(1, n_electrodes);
 slowAlphaBand = zeros(1, n_electrodes);
 gammaBand = zeros(1, n_electrodes);
+%default is 15 seconds long
+welchWindow = fs*15;
 
 for iElec = 1:n_electrodes
 	name = EEG_get_elname(iElec);
 	data = EEG_get_channel(EEGSignal, name);
 
 	%Welch method to compute energy of the current electrode (trial + BL)
-	[PowerTrial, fTrial] = pwelch(data, [], [], [], fs,'power');
-    deltaBand(iElec) = sum(PowerTrial(fTrial>0 & fTrial<4));
-	thetaBand(iElec) = sum(PowerTrial(4<=fTrial & fTrial<8));
-	slowAlphaBand(iElec) = sum(PowerTrial(8<=fTrial & fTrial<10));
-    alphaBand(iElec) = sum(PowerTrial(8<=fTrial & fTrial<12));
-	betaBand(iElec)  = sum(PowerTrial(12<=fTrial & fTrial<30));
-	gammaBand(iElec) = sum(PowerTrial(30<fTrial));
+	[PowerTrial, fTrial] = pwelch(data, welchWindow , [], [], fs);
+    deltaBand(iElec) = log(sum(PowerTrial(fTrial>0 & fTrial<4)));
+	thetaBand(iElec) = log(sum(PowerTrial(4<=fTrial & fTrial<8)));
+	slowAlphaBand(iElec) = log(sum(PowerTrial(8<=fTrial & fTrial<10)));
+    alphaBand(iElec) = log(sum(PowerTrial(8<=fTrial & fTrial<12)));
+	betaBand(iElec)  = log(sum(PowerTrial(12<=fTrial & fTrial<30)));
+	gammaBand(iElec) = log(sum(PowerTrial(30<fTrial)));
 end
 
 

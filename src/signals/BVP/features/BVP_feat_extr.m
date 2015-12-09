8cfbaafd2e7d48acb829b41d7ec6f47b0b5ef148
Mohammad Soleymani <mohammad.soleymani@unige.ch>
2015-12-09 16:17:53 +0100

    fixed the welch window sizes and added warnings

--- src/signals/BVP/features/BVP_feat_extr.m
+++ src/signals/BVP/features/BVP_feat_extr.m
@@ -75,7 +75,7 @@ if(~isempty(BVP_feats_names))
         mean_ = mean(rawSignal);
     end
     
-
+    
     
     if any(strcmp('meanIBI',BVP_feats_names)) || any(strcmp('HRV',BVP_feats_names))
         HRV = std(IBI);
@@ -86,35 +86,46 @@ if(~isempty(BVP_feats_names))
         [MSE] = multiScaleEntropy(IBI,5);
     end
     
-    if any(strncmp('sp',BVP_feats_names,2))
-        [P, f] = pwelch(rawSignal, [], [], [], samprate,'power');
-        P=P/sum(P);
-        %power spectral featyres
-        sp0001 = log(sum(P(f>0.0 & f<=0.1))+eps);
-        sp0102 = log(sum(P(f>0.1 & f<=0.2))+eps);
-        sp0203 = log(sum(P(f>0.2 & f<=0.3))+eps);
-        sp0304 = log(sum(P(f>0.3 & f<=0.4))+eps);
-        sp_energyRatio = log(sum(P(f<0.08))/sum(P(f>0.15 & f<5))+eps);
+    welch_window_size = fs*20;
+    if length(rawSignal)< welch_window_size +fs
+        warning('Signal is too short for this welch window size');
     end
-    
-    %tachogram features; psd features on inter beat intervals
-    %R. McCraty, M. Atkinson, W. Tiller, G. Rein, and A. Watkins, "The
-    %effects of emotions on short-term power spectrum analysis of
-    %heart rate variability," The American Journal of Cardiology, vol. 76,
-    %no. 14, pp. 1089 -1093, 1995
-    if any(strcmp('tachogram_LF',BVP_feats_names)) ...
-            || any(strcmp('tachogram_MF',BVP_feats_names)) ||  any(strcmp('tachogram_HF',BVP_feats_names)) ...
-            || any(strcmp('tachogram_energy_ratio',BVP_feats_names))
-        [Pt, ft] = pwelch(IBI, [], [], [], IBI_sp,'power');
-        clear tachogram %TODO: delete ? Why is it useful ?
-        %WARN: check that this is possible with the IBI sampling rate
-        %WARN: these values are sometimes negative because of the log, doesn't it appear as strange for a user ?
-        tachogram_LF = log(sum(Pt(ft>0.01 & ft<=0.08))+eps);
-        tachogram_MF = log(sum(Pt(ft>0.08 & ft<=0.15))+eps);
-        tachogram_HF = log(sum(Pt(ft>0.15 & ft<=0.5))+eps);
-        tachogram_energy_ratio = tachogram_MF/(tachogram_LF+tachogram_HF);
+    if length(rawSignal)< welch_window_size +1
+        warning('Signal is too short for this welch window size and the PSD features cannot be calculated');
+        sp0001 = NaN;sp0102 = NaN;sp0203 = NaN; sp0304 = NaN;sp_energyRatio = NaN;
+        tachogram_LF = NaN;tachogram_MF = NaN;tachogram_HF = NaN;
+        tachogram_energy_ratio = NaN;
+    else
+        
+        if any(strncmp('sp',BVP_feats_names,2))
+            [P, f] = pwelch(rawSignal, [], [], [], samprate,'power');
+            P=P/sum(P);
+            %power spectral featyres
+            sp0001 = log(sum(P(f>0.0 & f<=0.1))+eps);
+            sp0102 = log(sum(P(f>0.1 & f<=0.2))+eps);
+            sp0203 = log(sum(P(f>0.2 & f<=0.3))+eps);
+            sp0304 = log(sum(P(f>0.3 & f<=0.4))+eps);
+            sp_energyRatio = log(sum(P(f<0.08))/sum(P(f>0.15 & f<5))+eps);
+        end
+        
+        %tachogram features; psd features on inter beat intervals
+        %R. McCraty, M. Atkinson, W. Tiller, G. Rein, and A. Watkins, "The
+        %effects of emotions on short-term power spectrum analysis of
+        %heart rate variability," The American Journal of Cardiology, vol. 76,
+        %no. 14, pp. 1089 -1093, 1995
+        if any(strcmp('tachogram_LF',BVP_feats_names)) ...
+                || any(strcmp('tachogram_MF',BVP_feats_names)) ||  any(strcmp('tachogram_HF',BVP_feats_names)) ...
+                || any(strcmp('tachogram_energy_ratio',BVP_feats_names))
+            [Pt, ft] = pwelch(IBI, [], [], [], IBI_sp,'power');
+            clear tachogram %TODO: delete ? Why is it useful ?
+            %WARN: check that this is possible with the IBI sampling rate
+            %WARN: these values are sometimes negative because of the log, doesn't it appear as strange for a user ?
+            tachogram_LF = log(sum(Pt(ft>0.01 & ft<=0.08))+eps);
+            tachogram_MF = log(sum(Pt(ft>0.08 & ft<=0.15))+eps);
+            tachogram_HF = log(sum(Pt(ft>0.15 & ft<=0.5))+eps);
+            tachogram_energy_ratio = tachogram_MF/(tachogram_LF+tachogram_HF);
+        end
     end
-    
     %Write the values to the final vector output
     BVP_feats = [];
     for (i = 1:length(BVP_feats_names))

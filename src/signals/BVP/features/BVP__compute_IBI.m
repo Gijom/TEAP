cd44d2d878f7faa062d73790e1907de24e7addb8
Mohammad Soleymani <mohammad.soleymani@unige.ch>
2015-12-23 15:49:33 +0100

    fixed the trend problems and the bug

--- src/signals/BVP/features/BVP__compute_IBI.m
+++ src/signals/BVP/features/BVP__compute_IBI.m
@@ -16,10 +16,20 @@ if(isempty(Signal__get_raw(BVPSignal.IBI)))
 
     %Compute IBI
     newfs = 256; %Hz, as needed by rpeakdetect
-    BVP = resample(rawSignal, newfs, samprate); %WARN what happens if samprate/newfs is not a integer ?
-    BVP = detrend(BVP);
-    [hrv, R_t, R_amp, R_index, S_t, S_amp] = rpeakdetect(BVP', newfs);
+    BVP = resample(rawSignal, newfs, samprate); %WARN what happens if samprate/newfs is not a integer ?    
+    if size(BVP,1)<size(BVP,2)
+        BVP = BVP';
+    end    
+    %detrending
+    trend = smooth(BVP,newfs);
+    BVP =BVP - trend;
+    [hrv, R_t, R_amp, R_index, S_t, S_amp] = rpeakdetect(BVP, newfs);
     [~, IBI, ~, listePeak] = correctBPM(R_index, newfs);
+    % to debug and see the peaks
+    %pks = zeros(size(BVP))
+    %pks(R_index)=BVP(R_index);
+    %figure(22),plot((1:length(BVP)/newfs,pks.*BVP,'r*')
+    %figure(22),plot((1:length(BVP)/newfs,BVP)
     
     %Resample the signal with the one requested for IBI
     IBI_samprate = Signal__get_samprate(BVPSignal.IBI);

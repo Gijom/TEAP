8cec5325127d198767a288bf184616972ae230e2
Frank Villaro-Dixon <frank@vi-di.fr>
2016-05-25 22:55:23 +0200

    licence + licence notice

--- src/init.m
+++ src/init.m
@@ -7,6 +7,14 @@ folder(folder=='\')='/';
 addpath(genpath(folder))
 clear folder
 
+disp('TEAP loaded - Copyright 2016, TEAP contributors')
+disp('This program comes with ABSOLUTELY NO WARRANTY')
+disp('This is free software, and you are welcome to redistribute it')
+disp('under the GPLv2 licence')
+disp('Happy emotion analysis !')
+
+
+
 % The main documentation for TEAP, to be used by doxygen
 % \mainpage TEAP
 % \section What is TEAP ?
